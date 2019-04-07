8345c1bbb72a8136de104167109a32f401b25125
Raka <prakitarayyan@gmail.com>
2019-04-07 11:09:17 +0200

    Still subideal debug/graphical interface methods, OLR and PLR functions now

--- DPLL.py
+++ DPLL.py
@@ -23,13 +23,18 @@ def setCheck(c, varList):
             return False
     return True
 
-
+#Get the index of an element of a list
+def getIndex(list, el):
+    for i in range(len(list)):
+        if(el == list[i]):
+            return i
+    return -1
 
 def setVarTrue(clause, char):
     #Generation of trueBranch
     returnValue = []
     for lit in clause:
-        if(not setCheck(char, lit)):
+        if(not setCheck(char.lower(), lit)):
             continue
         temp = lit.replace(char.upper(), "")
         if(setCheck(temp, returnValue)):
@@ -42,9 +47,10 @@ def setVarFalse(clause, char):
     for lit in clause:
         if(not setCheck(char.upper(), lit)):
             continue
-        temp = lit.replace(char, "")
+        temp = lit.replace(char.lower(), "")
         if(setCheck(temp, returnValue)):
             returnValue.append(temp)
+    return returnValue
 
 
 #End Help-Methods--------
@@ -57,6 +63,7 @@ def setVarFalse(clause, char):
 def base_DPLL(clause, vars, depth, inter):
     #Checks whether to end Recursion
     if(len(clause)==0):
+        print("Done, A valid interpretation:")
         print(vars)
         print(inter)
         return True
@@ -75,6 +82,7 @@ def base_DPLL(clause, vars, depth, inter):
     #Print Debug
     #print(trueBranch)
     inter[depth] = 1
+    print(str(trueBranch) + ": " + vars[depth] + " = 1")
     if(DPLL_Step(trueBranch, vars, depth+1, inter)):
         return True
 
@@ -85,6 +93,7 @@ def base_DPLL(clause, vars, depth, inter):
     #print(falseBranch)
 
     inter[depth] = 0
+    print(str(falseBranch) + ": " + vars[depth] + " = 0")
     if(DPLL_Step(falseBranch, vars, depth+1, inter)):
         return True
 
@@ -96,19 +105,21 @@ def base_DPLL(clause, vars, depth, inter):
 def OLR(clause, vars, depth, inter):
     bool = False
     char = None
-    for lit in clause:
-        if(len(lit) == 1):
+    for i in range(len(clause)):
+        if(len(clause[i]) == 1):
             bool = True
-            char = lit[0]
+            char = clause[i][0]
             break
 
     if(bool):
         temp = []
         if(char.isupper()):
             temp = setVarFalse(clause, char)
+            inter[getIndex(vars, char.lower())] = 0
         else:
             temp = setVarTrue(clause, char)
-        print(temp)
+            inter[getIndex(vars, char.lower())] = 1
+        print(str(temp) + " OLR")
         return DPLL_Step(temp, vars, depth, inter)
     else:
         return PLR(clause, vars, depth, inter)
@@ -123,30 +134,35 @@ def PLR(clause, vars, depth, inter):
         for c in cl:
             if(setCheck(c, temp)):
                 temp.append(c)
+    temp = [c.lower() for c in temp]
     temp.sort()
-    for i in range(len(temp)):
+    i = 0
+    while(i<len(temp)):
         i_ = 1
-        while((i_+i) < len(temp) and temp[i+i_].lower() == temp[i].lower()):
+        while((i_+i) < len(temp) and temp[i+i_] == temp[i]):
             i_ = i_+1
         if(i_ == 1):
             bool = True
             char = temp[i]
             break
+        i = i+i_
 
     if(bool):
         temp = []
         if(char.isupper()):
             temp = setVarFalse(clause, char)
+            inter[getIndex(vars, char.lower())] = 0
         else:
             temp = setVarTrue(clause, char)
-        print(temp)
+            inter[getIndex(vars, char.lower())] = 1
+        print(str(temp) + " PLR")
         return DPLL_Step(temp, vars, depth, inter)
     else:
         return base_DPLL(clause, vars, depth, inter)
 
 #Because there are heuristics to consider, we will define the OLR->PLR->Base_DPLL sequencec as a step.
 def DPLL_Step(clause, vars, depth, inter):
-    OLR(clause, vars, depth, inter)
+    return OLR(clause, vars, depth, inter)
 
 
 #Initializing function
@@ -154,7 +170,8 @@ def DPLL(clause, vars):
     inter = []
     for v in vars:
         inter.append(-1) #-1 if an interpretation isnt assigned
-    DPLL_Step(clause, vars, 0, inter)
+    if not DPLL_Step(clause, vars, 0, inter):
+        print("Not solveable :(")
     return
 
 
@@ -167,4 +184,6 @@ for arg in args:
         if(setCheck(c, vars)):
             vars.append(c)
 vars.sort()
+
+print(str(args) + " Starting DPLL...")
 DPLL(args, vars)
